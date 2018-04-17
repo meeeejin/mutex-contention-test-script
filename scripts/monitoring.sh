bc4048282ad05092d2f2fe1ebb5c8acca72b38e2
meeeejin <meeeeejin@naver.com>
2018-04-17 14:15:49 +0900

    Added a test script

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,21 +1,24 @@
-# Remove the dump file which already exists
-rm /home/mijin/dump.out
+#!/bin/bash
 
-iostat -mx 1 > mutex_io.out &
+result_dir="/home/yangws/test_result"
+mysql_dir="/home/yangws/mysql-5.7.21"
+passwd="1234"
+
+end=$((SECONDS+3600))
 
 # Write mutex information to file every second
-while true
+while [ $SECONDS -lt $end ]
 do
-    # Get mutex information and write it to the dump file
-    mysql -uroot -pvldb7988 \
+    # Get mutex information and write it to the result file
+    mysql -uroot -p${passwd} \
         -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
             AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
             FROM performance_schema.events_waits_summary_global_by_event_name \
             WHERE SUM_TIMER_WAIT > 0 AND \
             EVENT_NAME LIKE 'wait/synch/mutex/innodb/%' \
             or EVENT_NAME LIKE 'wait/synch/rwlock/innodb/%' \
-            ORDER BY SUM_TIMER_WAIT_MS DESC;" >> dump.out
-    echo -e "" >> dump.out
+            ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/mutex_buf_$1.out
+    echo -e "" >> ${result_dir}/mutex_buf_$1.out
     
     sleep 1
 done
---
+++ scripts/runTest.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+result_dir="/home/yangws/test_result"
+data_dir="/home/yangws/test_data"
+log_dir="/home/yangws/test_log"
+backup_dir="/home/yangws/data_backup"
+mysql_dir="/home/yangws/mysql-5.7.21"
+tpcc_dir="/home/yangws/tpcc-mysql"
+
+buf_instance=( "1" "4" "8" "16" "32" )
+passwd="1234"
+
+echo "Start TPCC to test mutex contention"
+
+for i in "${buf_instance[@]}"
+do
+    # If the result file already exists, remove it
+    rm ${result_dir}/mutex_buf_${i}.out
+
+    # Remove existing data
+    rm -rf ${data_dir}/*
+    rm -rf ${log_dir}/*
+
+    # Copy data
+    cp -r ${backup_dir}/data/* ${data_dir}/
+    cp -r ${backup_dir}/log/* ${log_dir}/
+
+    # Run MySQL server
+    echo "RUN MYSQL SERVER"
+    cd ${mysql_dir}
+    ./bin/mysqld_safe --innodb_buffer_pool_instances=$i &>/dev/null &disown
+
+    sleep 5s
+
+    # Run TPC-C benchmark
+    echo "START TPCC BENCHMARK"
+    cd ${tpcc_dir}
+    iostat -mx 1 > ${result_dir}/iostat_buf_${i}.out &
+    ./runBench.sh | tee ${result_dir}/tpcc_buf_${i}.out
+
+    killall -9 iostat
+
+    # Shutdown MySQL server
+    ps cax | grep mysql > /dev/null
+    if [ $? -eq 0 ]; then
+        cd ${mysql_dir}
+        ./bin/mysqladmin -uroot -p${passwd} shutdown
+    fi
+done
+
+
